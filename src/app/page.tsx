8a94994aaa0f784b3504c643b46eee87767def4a
harumaki1220 <haruma181220@gmail.com>
2025-06-05 13:58:01 +0900

    ボムマップにボムをランダムに配置.次はcalcBoard.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,36 +2,11 @@
 
 import { useState } from 'react';
 import styles from './page.module.css';
+let count = 0;
+// ユーザーの操作、爆弾の位置
+// userInputs,bombMap,
+const calcBoard = (userInputs: number[][], bombMap: number[][]) => {};
 
-const calcTotalPoint = (arr: number[], counter: number) => {
-  const total = arr.reduce((k, i) => {
-    return k + i;
-  });
-  return total + counter;
-};
-const down = (n: number) => {
-  if (n >= 0) {
-    console.log(n);
-    down(n - 1);
-  }
-};
-down(10);
-const sum1 = (n: number): number => {
-  if (n > 0) {
-    return n + sum1(n - 1);
-  } else {
-    return n;
-  }
-};
-console.log(sum1(10));
-const sum2 = (n: number, n1: number): number => {
-  return n < n1 ? n + sum2(n + 1, n1) : n;
-};
-console.log(sum2(4, 10)); //49
-const sum3 = (n: number, n1: number): number => {
-  return ((n1 - n + 1) / 2) * (n + n1);
-};
-console.log(sum3(4, 10)); //49
 export default function Home() {
   const [bombMap, setbombMap] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -44,6 +19,7 @@ export default function Home() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  //0:セーフ, 1:爆弾
 
   const [userInputs, setuserInputs] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -59,9 +35,33 @@ export default function Home() {
   // 0:透明, 1:開ける, 2:旗, 3:はてな,の予定
 
   const leftclick = (x: number, y: number) => {
+    let rx = 0;
+    let ry = 0;
+    let bombcount = 0;
+    const newbombMap = structuredClone(bombMap);
     console.log(x, y);
     const newuserInputs = structuredClone(userInputs);
-    newuserInputs[y][x] = 1;
+    if (newuserInputs[y][x] === 0) {
+      newuserInputs[y][x] = 1;
+      count += 1;
+    }
+    if (count === 1) {
+      while (bombcount < 11) {
+        rx = Math.floor(Math.random() * 9);
+        ry = Math.floor(Math.random() * 9);
+        console.log(bombcount);
+        if (newbombMap[ry][rx] === 0) {
+          newbombMap[ry][rx] = 1;
+          bombcount += 1;
+        }
+        if (bombcount === 10) {
+          console.log(newbombMap);
+          setbombMap(newbombMap);
+          break;
+        }
+      }
+    }
+
     setuserInputs(newuserInputs);
   };
 
@@ -79,7 +79,13 @@ export default function Home() {
     setuserInputs(newuserInputs);
   };
 
-  console.log(userInputs);
+  const calcBoard = (userInputs: number[][], bombMap: number[][]) => {
+    const newBoard = Array.from({ length: userInputs.length }, () =>
+      Array.from({ length: userInputs[0].length }, () => 0),
+    );
+  };
+  const board = calcBoard(userInputs, bombMap);
+
   return (
     <div className={styles.container}>
       <div className={styles.board}>
@@ -97,8 +103,23 @@ export default function Home() {
             />
           )),
         )}
+        {bombMap.map((row, y) =>
+          row.map((value, x) => (
+            <div
+              className={styles.undercell}
+              key={`${x}-${y}`}
+              onClick={() => leftclick(x, y)}
+              onContextMenu={(event) => rightclick(x, y, event)}
+              style={{
+                backgroundPosition: `${value === 1 ? -300 : 30}px`,
+              }}
+            />
+          )),
+        )}
       </div>
       {/* <button onClick={clickHandler}>クリック</button> */}
     </div>
   );
 }
+
+// 再起関数
